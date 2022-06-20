3ad5f12baaa6ad60da5a0e86e2fdfd318d6393b6
Owlcept <owlcept@owlcept.com>
2022-06-20 10:12:32 -0500

    Testing with different tweet

--- testing.py
+++ testing.py
@@ -1,21 +1,20 @@
 #Playwright playground
-import time
 from playwright.sync_api import sync_playwright, ViewportSize
 
 with sync_playwright() as p:
     browser = p.firefox.launch()
     context = browser.new_context()
     page = context.new_page()
-    page.goto('https://twitter.com/ardentreflexion/status/1537471416467177474')
-    #                                                     1537501373100589056
+    page.goto('https://twitter.com/domiono/status/1538891392583450629')
+ 
     #This was pure testing, will be removed in final
-    page.locator('data-testid=tweet').screenshot(path="./title.png")
+    page.locator('data-testid=tweet').screenshot(path="twitter_scrape/title.png")
     x = page.locator('data-testid=tweet')
     y = page.locator('data-testid=tweetText')
     print(x.count())
-    for i in range(x.count()):
+    for i in range(1,9):
         #This colects the text(does not grab emoji
         print(y.nth(i).text_content())
-        x.nth(i).screenshot(path = f'./title{i}.png')
+        x.nth(i).screenshot(path = f'./twitter_scrape/title{i}.png')
     browser.close()
 
