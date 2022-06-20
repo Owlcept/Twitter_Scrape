bf24df2063c2b63966c110b7fef970f97fc008d8
Drew Lewis <67299472+Owlcept@users.noreply.github.com>
2022-06-19 22:48:24 -0500

    Added some text for information

--- testing.py
+++ testing.py
@@ -8,11 +8,13 @@ with sync_playwright() as p:
     page = context.new_page()
     page.goto('https://twitter.com/ardentreflexion/status/1537471416467177474')
     #                                                     1537501373100589056
+    #This was pure testing, will be removed in final
     page.locator('data-testid=tweet').screenshot(path="./title.png")
     x = page.locator('data-testid=tweet')
     y = page.locator('data-testid=tweetText')
     print(x.count())
     for i in range(x.count()):
+        #This colects the text(does not grab emoji
         print(y.nth(i).text_content())
         x.nth(i).screenshot(path = f'./title{i}.png')
     browser.close()
