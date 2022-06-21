c71a698d9963b24dacae78f9bfd1e11767dca6fb
Owlcept <owlcept@owlcept.com>
2022-06-21 16:22:51 -0500

    Add explanations

--- testing.py
+++ testing.py
@@ -1,19 +1,24 @@
-#Playwright playground
+#Created by: Owlcept
 from playwright.sync_api import sync_playwright
 
 def twitter_scrape(tweet, num_com=9):
+
     with sync_playwright() as p:
+        #Must use firefox // Other chromium wont load tweets
         browser = p.firefox.launch()
         context = browser.new_context()
         page = context.new_page()
         page.goto(tweet)
+
+        #Debug tweet below
         #page.goto('https://twitter.com/domiono/status/1538891392583450629')
     
-        #This was pure testing, will be removed in final
+        #Title Screenshot
         page.locator('data-testid=tweet').screenshot(path="twitter_scrape/title.png")
         x = page.locator('data-testid=tweet')
         y = page.locator('data-testid=tweetText')
     
+        #Use 1 as start for skipping title
         for i in range(1,num_com):
             if i==3:
                 #Skip the 3rd screenshot because of signup banner
@@ -21,6 +26,6 @@ def twitter_scrape(tweet, num_com=9):
             #This colects the text(does not grab emoji
 
             print(y.nth(i).text_content())
-            x.nth(i).screenshot(path = f'./twitter_scrape/title{i}.png')
+            x.nth(i).screenshot(path = f'./assets/title{i}.png')
         browser.close()
 
